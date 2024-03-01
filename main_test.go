2784cda253c5ab3d76efa4a8731e51066fbe9e26
xibo <xibo@portkey.finance>
2024-03-01 11:00:19 +0800

    feat/add-traits-folder

--- main_test.go
+++ main_test.go
@@ -11,10 +11,6 @@ import (
 
 func TestName(t *testing.T) {
 
-	//一人之下宝儿姐，汉服婚礼红色服饰，淡颜系，清纯感，韩系,背景是中国古代长安街
-	//requestMsg := "一人之下宝儿姐，汉服，淡颜系，清纯感，韩系,背景是中国古代长安街"
-	//requestMsg := "一人之下宝儿姐，汉服，淡颜系，清纯感，高冷，韩系,背景是中国古代长安街"
-	//requestMsg := "一人之下宝儿姐，秘书制服，淡颜系，清纯感，御姐，韩系"
 
 	config, _ := service.ReadConfig()
 	Url := "https://www.bing.com/images/create/async/results/1-657aea27004e4180a3c3500267147c8d?q=%E5%A1%9E%E5%B0%94%E8%BE%BE%E5%85%AC%E4%B8%BB%EF%BC%8C%E6%B7%A1%E9%A2%9C%E7%B3%BB%EF%BC%8C%E6%B8%85%E7%BA%AF%E6%84%9F%EF%BC%8C%E9%9F%A9%E7%B3%BB"

a9edef1ceb1fd799d140e82270dccfa34ff0cf99
xibo <xibo@portkey.finance>
2024-03-01 17:09:44 +0800

    feat/remove-mongo

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -1,38 +1,11 @@
 package handler
 
 import (
-	"context"
 	"github.com/gin-gonic/gin"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
 	"image-Designer/internal/service"
-	"go.mongodb.org/mongo-driver/bson"
-	"log"
 	"net/http"
 	"time"
 )
-
-var (
-	mongoClient *mongo.Client
-)
-
-func init() {
-	// Set up MongoDB connection
-	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-	var err error
-	mongoClient, err = mongo.Connect(ctx, clientOptions)
-	if err != nil {
-		log.Fatal(err)
-	}
-	// Ping the MongoDB to check the connection
-	err = mongoClient.Ping(ctx, nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Println("Connected to MongoDB!")
-}
 
 func SubmitResultHandler(c *gin.Context) {
 	requestMsg := c.Query("message")
@@ -67,26 +40,9 @@ func SubmitResultHandler(c *gin.Context) {
 		return
 	}
 
-	// Store data in MongoDB
-	err = storeDataInMongoDB(result)
-	if err != nil {
-		log.Println("Error storing data in MongoDB:", err)
-	}
-
 	c.JSON(http.StatusOK, gin.H{
 		"message": "Success",
 		"data":    result,
 		"code":    200,
 	})
-}
-
-func storeDataInMongoDB(data []string) error {
-	collection := mongoClient.Database("cats").Collection("generated_img")
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-	_, err := collection.InsertOne(ctx, bson.M{"data": data})
-	if err != nil {
-		return err
-	}
-	return nil
 }
